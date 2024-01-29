b441fef6b2648a1333bd7f9feb1badf9a0bd23b7
Cannon Tuttle <c>
2024-01-29 15:49:16 -0700

    working android dev build

--- build_android.sh
+++ build_android.sh
@@ -1,6 +1,6 @@
 docker run \
   --rm \
   -v $(pwd):/root/src \
-  -v /tmp/registry\":/usr/local/cargo/registry\" \
+  -v /tmp/registry:/usr/local/cargo/registry \
   -w /root/src \
   notfl3/cargo-apk cargo quad-apk build --release

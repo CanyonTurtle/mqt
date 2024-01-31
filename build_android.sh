11bdbf584c501e2ffdb1f1fb5bd11eeaede885f9
Cannon Tuttle <c>
2024-01-30 21:29:54 -0700

    working docker android

--- build_android.sh
+++ build_android.sh
@@ -3,4 +3,4 @@ docker run \
   -v $(pwd):/root/src \
   -v /tmp/registry:/usr/local/cargo/registry \
   -w /root/src \
-  notfl3/cargo-apk cargo quad-apk build --release
+  notfl3/cargo-apk /bin/bash -c "rustup target add armv7-linux-androideabi && cargo quad-apk build --release --package mqt"
